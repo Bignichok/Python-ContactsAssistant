56509eef1a171597bccff5707af909755567baa1
Nikita-Samoilenko <mykyta.samoilenko@gmail.com>
2024-05-21 15:25:52 +0300

    dataHelpers docs

--- src/ContactsAssistant/dataHelpers.py
+++ src/ContactsAssistant/dataHelpers.py
@@ -5,12 +5,39 @@ from AddressBook import AddressBook
 FILENAME = "./src/ContactsAssistant/addressbook.pkl"
 
 def save_data(book, filename=FILENAME):
-    with open(filename, "wb") as file:
-        pickle.dump(book, file)
+    """
+    Saves the address book data to a file using pickle.
+
+    Args:
+        book (AddressBook): The address book object to be saved.
+        filename (str): The name of the file where the data will be saved. Defaults to FILENAME.
+
+    Raises:
+        Exception: If there is an error during the file operation.
+    """
+    try:
+        with open(filename, "wb") as file:
+            pickle.dump(book, file)
+    except Exception as e:
+        raise Exception(f"Error saving data: {e}")
 
 def load_data(filename=FILENAME):
+    """
+    Loads the address book data from a file using pickle.
+
+    Args:
+        filename (str): The name of the file from which the data will be loaded. Defaults to FILENAME.
+
+    Returns:
+        AddressBook: The loaded address book object. If the file does not exist, returns a new AddressBook instance.
+
+    Raises:
+        Exception: If there is an error during the file operation other than FileNotFoundError.
+    """
     try:
         with open(filename, "rb") as file:
             return pickle.load(file)
     except FileNotFoundError:
         return AddressBook()
+    except Exception as e:
+        raise Exception(f"Error loading data: {e}")
