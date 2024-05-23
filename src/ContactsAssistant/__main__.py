c86c8c64b8cc3bceefd71636e2c42064f0cc31ec
varstab <dan.vartsab@gmail.com>
2024-05-23 13:51:03 -0400

    integrated the notebook into the main menu

--- src/ContactsAssistant/__main__.py
+++ src/ContactsAssistant/__main__.py
@@ -9,6 +9,8 @@ from dataHelpers import save_data, load_data
 from prompt_toolkit import prompt
 from prompt_toolkit.styles import Style
 from contactcompleter import ContactCompleter
+from notebook import Notebook
+from note import Note
 
 
 NOT_FOUND_MESSAGE = "Contact does not exist, you can add it"
@@ -228,6 +230,209 @@ def get_upcoming_birthdays(args, book: ContactsBook):
         return book.get_upcoming_birthdays()
 
 
+@handle_error
+def add_note_prompt(args, notebook: Notebook):
+    """
+    Add a note to the notebook via user prompt.
+
+    Args:
+        args (list): List of arguments.
+        notebook (Notebook): The notebook to add the note to.
+
+    Returns:
+        str: Message indicating whether the note was added or not.
+    """
+    title = input("Enter title: ")
+    content = input("Enter content: ")
+    tags = input("Enter tags (comma-separated): ").split(",")
+    due_date = input("Enter due date (DD.MM.YYYY, optional): ").strip()
+    due_date = due_date if due_date else None
+    try:
+        note = Note(title=title, content=content, tags=[tag.strip() for tag in tags], due_date=due_date)
+        return notebook.add(note)
+    except ValueError as e:
+        return str(e)
+
+@handle_error
+def add_note_json(args, notebook: Notebook):
+    """
+    Add a note to the notebook from a JSON object.
+
+    Args:
+        args (list): List containing the JSON object.
+        notebook (Notebook): The notebook to add the note to.
+
+    Returns:
+        str: Message indicating whether the note was added or not.
+    """
+    if len(args) < 1:
+        return "Please provide the JSON object for the note."
+
+    json_obj = " ".join(args)
+    note = Note.from_json(json_obj)
+    return notebook.add(note)
+
+@handle_error
+def show_note(args, notebook: Notebook):
+    """
+    Show the details of a note.
+
+    Args:
+        args (list): List containing the title of the note.
+        notebook (Notebook): The notebook containing the note.
+
+    Returns:
+        str or Note: The note's details or a message indicating the note was not found.
+    """
+    if len(args) < 1:
+        return "Please provide the title of the note."
+
+    title = args[0]
+    notes = notebook.search(title)
+    if notes:
+        return notebook.format_notes_with_frame(notes)
+    else:
+        return f"Note {title} not found."
+
+@handle_error
+def delete_note(args, notebook: Notebook):
+    """
+    Delete a note from the notebook.
+
+    Args:
+        args (list): List containing the title of the note.
+        notebook (NoteBook): The notebook containing the note.
+
+    Returns:
+        str: Message indicating whether the note was deleted or not.
+    """
+    if len(args) < 1:
+        return "Please provide the title of the note."
+
+    title = args[0]
+    return notebook.remove(title)
+
+@handle_error
+def delete_all_notes(args, notebook: Notebook):
+    """
+    Delete all notes from the notebook.
+
+    Args:
+        args (list): List of arguments.
+        notebook (Notebook): The notebook containing the notes.
+
+    Returns:
+        str: Message indicating whether the notes were deleted or not.
+    """
+    return notebook.remove_all()
+
+@handle_error
+def update_note_prompt(args, notebook: Notebook):
+    """
+    Update a note in the notebook via user prompt.
+
+    Args:
+        args (list): List of arguments.
+        notebook (Notebook): The notebook containing the note.
+
+    Returns:
+        str: Message indicating whether the note was updated or not.
+    """
+    if len(args) < 1:
+        return "Please provide the title of the note to update."
+
+    title = args[0]
+    content = input("Enter new content: ")
+    tags = input("Enter new tags (comma-separated): ").split(",")
+    due_date = input("Enter new due date (DD.MM.YYYY, optional): ").strip()
+    due_date = due_date if due_date else None
+    try:
+        new_note = Note(title=title, content=content, tags=[tag.strip() for tag in tags], due_date=due_date)
+        return notebook.update(title, new_note)
+    except ValueError as e:
+        return str(e)
+
+@handle_error
+def search_notes(args, notebook: Notebook):
+    """
+    Search for notes containing the query in their title or content.
+
+    Args:
+        args (list): List containing the search query.
+        notebook (Notebook): The notebook containing the notes.
+
+    Returns:
+        str: Search results or a message indicating no notes were found.
+    """
+    if len(args) < 1:
+        return "Please provide the search query."
+
+    query = args[0]
+    results = notebook.search(query)
+    if results:
+        return notebook.format_notes_with_frame(results)
+    else:
+        return f"No notes found containing '{query}'."
+
+@handle_error
+def filter_notes(args, notebook: Notebook):
+    """
+    Filter notes by tag.
+
+    Args:
+        args (list): List containing the tag to filter by.
+        notebook (Notebook): The notebook containing the notes.
+
+    Returns:
+        str: Filter results or a message indicating no notes were found.
+    """
+    if len(args) < 1:
+        return "Please provide the tag to filter by."
+
+    tag = args[0]
+    results = notebook.filter_by_tag(tag)
+    if results:
+        return notebook.format_notes_with_frame(results)
+    else:
+        return f"No notes found with tag '{tag}'."
+
+@handle_error
+def notes_due(args, notebook: Notebook):
+    """
+    Get notes that are due in the next specified number of days.
+
+    Args:
+        args (list): List containing the number of days to look ahead.
+        notebook (Notebook): The notebook containing the notes.
+
+    Returns:
+        str: List of due notes or a message indicating no notes are due.
+    """
+    if len(args) < 1:
+        return "Please provide the number of days to look ahead for due notes."
+    
+    days = int(args[0])
+    results = notebook.notes_due_in_days(days)
+    if results:
+        return notebook.format_notes_with_frame(results)
+    else:
+        return f"No notes due in the next {days} days."
+
+@handle_error
+def print_all_notes(args, notebook: Notebook):
+    """
+    Print all notes in the notebook.
+
+    Args:
+        args (list): List of arguments.
+        notebook (Notebook): The notebook containing the notes.
+
+    Returns:
+        str: String representation of all notes.
+    """
+    return notebook.print_all_notes()
+
+
 def parse_input(user_input):
     """
     Parse user input into a command and its arguments.
@@ -244,6 +449,45 @@ def parse_input(user_input):
 
 
 def main():
+
+    notebook = Notebook.load_from_file("notebook.json")
+
+    # Adding 5 dummy notes for the demo purposes.
+    dummy_notes = [
+        Note(
+            title="Grocery Shopping",
+            content="Buy fruits and vegetables",
+            tags=["shopping", "errand"],
+            due_date="20.05.2024"
+        ),
+        Note(
+            title="Doctor Appointment",
+            content="Annual checkup at the clinic",
+            tags=["health", "appointment"],
+            due_date="22.05.2024"
+        ),
+        Note(
+            title="Project Deadline",
+            content="Submit the final project report",
+            tags=["work", "important"],
+            due_date="25.05.2024"
+        ),
+        Note(
+            title="Birthday Party",
+            content="Celebrate John's birthday at his house",
+            tags=["party", "celebration"],
+            due_date="21.05.2024"
+        ),
+        Note(
+            title="Yoga Class",
+            content="Morning yoga session at the gym",
+            tags=["exercise", "health"],
+            due_date="23.05.2024"
+        )
+    ]
+    for note in dummy_notes:
+        notebook.add(note, suppress_message=True)
+
     """
     Main function to run the assistant bot.
 
@@ -288,6 +532,24 @@ def main():
                 print(*get_upcoming_birthdays(args, book), sep="\n")
             case "update_contact_email":
                 print(update_contact_email(args, book))
+            case "add-note":
+                print(add_note_prompt(args, notebook))
+            case "find-note":
+                print(show_note(args, notebook))
+            case "update-note":
+                print(update_note_prompt(args, notebook))
+            case "delete-note":
+                print(delete_note(args, notebook))
+            case "delete-all-notes":
+                print(delete_all_notes(args, notebook))
+            case "search-notes":
+                print(search_notes(args, notebook))
+            case "filter-notes":
+                print(filter_notes(args, notebook))
+            case "notes-due":
+                print(notes_due(args, notebook))
+            case "all-notes":
+                print(print_all_notes(args, notebook))
             case "help":
                 print("Allowed commands:")
                 print(*Menu.get_commands_list(), sep="\n")
